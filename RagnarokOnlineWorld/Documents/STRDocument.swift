e89e53fbf3f90b13c3f23b610a4245ab17f54f08
Leon Li <arkadeleon@gmail.com>
2020-05-19 17:33:29 +0800

    Add STRDocument

---
+++ RagnarokOnlineWorld/Documents/STRDocument.swift
@@ -0,0 +1,120 @@
+//
+//  STRDocument.swift
+//  RagnarokOnlineWorld
+//
+//  Created by Li, Junlin on 2020/5/19.
+//  Copyright © 2020 Leon & Vane. All rights reserved.
+//
+
+import Foundation
+
+struct STRLayer {
+
+    var texcnt: Int32
+    var texname: [String]
+    var anikeynum: Int32
+    var animations: [STRAnimation]
+}
+
+struct STRAnimation {
+
+    var frame: Int32
+    var type: UInt32
+    var pos: Vector2<Float>
+    var uv: [Float]
+    var xy: [Float]
+    var aniframe: Float
+    var anitype: UInt32
+    var delay: Float
+    var angle: Float
+    var color: Vector4<Float>
+    var srcalpha: UInt32
+    var destalpha: UInt32
+    var mtpreset: UInt32
+}
+
+class STRDocument: Document {
+
+    private var reader: BinaryReader!
+
+    private(set) var header = ""
+    private(set) var version: UInt32 = 0
+    private(set) var fps: UInt32 = 0
+    private(set) var maxKey: UInt32 = 0
+    private(set) var layernum: UInt32 = 0
+    private(set) var layers: [STRLayer] = []
+
+    override func load(from contents: Data) throws {
+        let stream = DataStream(data: contents)
+        reader = BinaryReader(stream: stream)
+
+        header = try reader.readString(count: 4)
+        guard header == "STRM" else {
+            throw StreamError.invalidContents
+        }
+
+        version = try reader.readUInt32()
+        guard version == 0x94 else {
+            throw StreamError.invalidContents
+        }
+
+        fps = try reader.readUInt32()
+        maxKey = try reader.readUInt32()
+        layernum = try reader.readUInt32()
+
+        try reader.skip(count: 16)
+
+        layers = []
+        for _ in 0..<layernum {
+            let layer = try readLayer()
+            layers.append(layer)
+        }
+
+        reader = nil
+    }
+
+    private func readLayer() throws -> STRLayer {
+        let texcnt = try reader.readInt32()
+
+        var texname: [String] = []
+        for _ in 0..<texcnt {
+            let name = try "data\\texture\\effect\\" + reader.readString(count: 128)
+            texname.append(name)
+        }
+
+        let anikeynum = try reader.readInt32()
+
+        var animations: [STRAnimation] = []
+        for _ in 0..<anikeynum {
+            let animation = try readAnimation()
+            animations.append(animation)
+        }
+
+        let layer = STRLayer(
+            texcnt: texcnt,
+            texname: texname,
+            anikeynum: anikeynum,
+            animations: animations
+        )
+        return layer
+    }
+
+    private func readAnimation() throws -> STRAnimation {
+        let animation = try STRAnimation(
+            frame: reader.readInt32(),
+            type: reader.readUInt32(),
+            pos: [reader.readFloat32(), reader.readFloat32()],
+            uv: [reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32()],
+            xy: [reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32(), reader.readFloat32()],
+            aniframe: reader.readFloat32(),
+            anitype: reader.readUInt32(),
+            delay: reader.readFloat32(),
+            angle: reader.readFloat32() / (1024 / 360),
+            color: [reader.readFloat32() / 255, reader.readFloat32() / 255, reader.readFloat32() / 255, reader.readFloat32() / 255],
+            srcalpha: reader.readUInt32(),
+            destalpha: reader.readUInt32(),
+            mtpreset: reader.readUInt32()
+        )
+        return animation
+    }
+}
