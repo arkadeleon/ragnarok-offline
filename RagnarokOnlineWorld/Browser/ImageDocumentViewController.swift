489a407bf2bc7c7e333a0f62ac5f57d4e72229f0
Leon Li <arkadeleon@gmail.com>
2020-06-18 23:26:29 +0800

    Update image document view

--- RagnarokOnlineWorld/Browser/ImageDocumentViewController.swift
+++ RagnarokOnlineWorld/Browser/ImageDocumentViewController.swift
@@ -31,21 +31,67 @@ class ImageDocumentViewController: UIViewController {
 
         view.backgroundColor = .systemBackground
 
-        scrollView = UIScrollView(frame: view.bounds)
-        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.delegate = self
-        scrollView.minimumZoomScale = 1
-        scrollView.maximumZoomScale = 3
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
         view.addSubview(scrollView)
 
-        imageView = UIImageView(frame: view.bounds)
+        imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         scrollView.addSubview(imageView)
 
+        scrollView.frameLayoutGuide.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        scrollView.frameLayoutGuide.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        scrollView.frameLayoutGuide.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.frameLayoutGuide.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+
         document.open { _ in
-            self.imageView.image = self.document.image
-            self.document.close()
+            defer {
+                self.document.close()
+            }
+
+            guard let image = self.document.image else {
+                return
+            }
+
+            self.imageView.image = image
+            self.imageView.frame = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+
+            self.scrollView.contentSize = image.size
+
+            self.updateZoomScale(image: image)
+            self.centerScrollViewContents()
         }
+    }
+
+    private func updateZoomScale(image: UIImage) {
+        let scrollViewFrame = scrollView.bounds
+
+        let scaleWidth = scrollViewFrame.size.width / image.size.width
+        let scaleHeight = scrollViewFrame.size.height / image.size.height
+        let minScale = min(scaleWidth, scaleHeight)
+
+        scrollView.minimumZoomScale = minScale
+        scrollView.maximumZoomScale = minScale * 3
+
+        scrollView.zoomScale = minScale
+    }
+
+    private func centerScrollViewContents() {
+        var horizontalInset: CGFloat = 0
+        var verticalInset: CGFloat = 0
+
+        if scrollView.contentSize.width < scrollView.bounds.width {
+            horizontalInset = (scrollView.bounds.width - scrollView.contentSize.width) * 0.5
+        }
+
+        if scrollView.contentSize.height < scrollView.bounds.height {
+            verticalInset = (scrollView.bounds.height - scrollView.contentSize.height) * 0.5
+        }
+
+        scrollView.contentInset = UIEdgeInsets(top: verticalInset, left: horizontalInset, bottom: verticalInset, right: horizontalInset)
     }
 }
 
@@ -53,5 +99,9 @@ extension ImageDocumentViewController: UIScrollViewDelegate {
 
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
+    }
+
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerScrollViewContents()
     }
 }
