c73785f394b2269adc0b8d11df6fe422c9958d48
Leon Li <arkadeleon@gmail.com>
2024-04-11 17:02:41 +0800

    Remove PacketVersion struct

--- Sources/RONetwork/PacketVersion.swift
+++ Sources/RONetwork/PacketVersion.swift
@@ -19,65 +19,23 @@ public let PACKET_VERSION_MAIN_NUMBER = !PACKET_VERSION_RE ? PACKET_VERSION : ni
 /// See ``PACKETVER_RE_NUM``
 public let PACKET_VERSION_RE_NUMBER = PACKET_VERSION_RE ? PACKET_VERSION : nil
 
-public struct PacketVersion {
+/// See ``PACKET_OBFUSCATION``
+public let PACKET_VERSION_OBFUSCATION = PACKET_VERSION >= 20110817
 
-    /// See ``PACKETVER``
-    public let number: Int
+/// See ``OFFICIAL_GUILD_STORAGE``
+public let PACKET_VERSION_OFFICIAL_GUILD_STORAGE = PACKET_VERSION >= 20131223
 
-    /// See ``PACKETVER_RE``
-    public var supportsRE: Bool {
-        if (number > 20151104 && number < 20180704) ||
-            (number >= 20200902 && number <= 20211118) {
-            true
-        } else {
-            false
-        }
-    }
+/// See ``PACKETVER_SUPPORTS_PINCODE``
+public let PACKET_VERSION_SUPPORTS_PINCODE = PACKET_VERSION >= 20110309
 
-    /// See ``PACKETVER_MAIN_NUM``
-    public var mainNumber: Int? {
-        !supportsRE ? number : nil
-    }
+/// See ``PACKETVER_CHAR_DELETEDATE``
+public let PACKET_VERSION_CHAR_DELETEDATE = (PACKET_VERSION > 20130000 && PACKET_VERSION <= 20141022) || PACKET_VERSION >= 20150513
 
-    /// See ``PACKETVER_RE_NUM``
-    public var reNumber: Int? {
-        supportsRE ? number : nil
-    }
+/// See ``PACKETVER_SUPPORTS_SALES``
+public let PACKET_VERSION_SUPPORTS_SALES = PACKET_VERSION >= 20131223
 
-    /// See ``PACKET_OBFUSCATION``
-    public var supportsObfuscation: Bool {
-        number >= 20110817
-    }
-
-    /// See ``OFFICIAL_GUILD_STORAGE``
-    public var supportsOfficialGuildStorage: Bool {
-        number >= 20131223
-    }
-
-    /// See ``PACKETVER_SUPPORTS_PINCODE``
-    public var supportsPincode: Bool {
-        number >= 20110309
-    }
-
-    /// See ``PACKETVER_CHAR_DELETEDATE``
-    public var supportsCharDeleteRemainingTime: Bool {
-        (number > 20130000 && number <= 20141022) || number >= 20150513
-    }
-
-    /// See ``PACKETVER_SUPPORTS_SALES``
-    public var supportsSales: Bool {
-        number >= 20131223
-    }
-
-    /// See ``WEB_SERVER_ENABLE``
-    public var supportsWebServer: Bool {
-        number > 20200300
-    }
-
-    public init(number: Int) {
-        self.number = number
-    }
-}
+/// See ``WEB_SERVER_ENABLE``
+public let PACKET_VERSION_SERVER_ENABLE = PACKET_VERSION > 20200300
 
 //func < (lhs: Int?, rhs: Int) -> Bool {
 //    if let lhs {
