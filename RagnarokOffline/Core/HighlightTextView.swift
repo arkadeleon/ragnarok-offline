33ecefecc28295a89906611071357f9f613d8a5f
Leon Li <arkadeleon@gmail.com>
2025-03-19 10:29:05 +0800

    Set highlight style to "github"

--- RagnarokOffline/Core/HighlightTextView.swift
+++ RagnarokOffline/Core/HighlightTextView.swift
@@ -14,12 +14,25 @@ import WebKit
 struct HighlightTextView: NSViewRepresentable {
     let text: String
 
+    @Environment(\.colorScheme) private var colorScheme
+
+    private var style: String {
+        switch colorScheme {
+        case .light:
+            "github"
+        case .dark:
+            "github-dark"
+        @unknown default:
+            "default"
+        }
+    }
+
     func makeNSView(context: Context) -> WKWebView {
         WKWebView()
     }
 
     func updateNSView(_ webView: WKWebView, context: Context) {
-        webView.highlightText(text)
+        webView.highlightText(text, style: style)
     }
 }
 
@@ -28,12 +41,25 @@ struct HighlightTextView: NSViewRepresentable {
 struct HighlightTextView: UIViewRepresentable {
     let text: String
 
+    @Environment(\.colorScheme) private var colorScheme
+
+    private var style: String {
+        switch colorScheme {
+        case .light:
+            "github"
+        case .dark:
+            "github-dark"
+        @unknown default:
+            "default"
+        }
+    }
+
     func makeUIView(context: Context) -> WKWebView {
         WKWebView()
     }
 
     func updateUIView(_ webView: WKWebView, context: Context) {
-        webView.highlightText(text)
+        webView.highlightText(text, style: style)
     }
 }
 
