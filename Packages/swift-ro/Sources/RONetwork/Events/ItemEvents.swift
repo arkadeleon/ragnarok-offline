388289ca84344deaff57d756e24ff81477b93401
Leon Li <arkadeleon@gmail.com>
2025-04-18 17:05:10 +0800

    Updated item event structure

--- Packages/swift-ro/Sources/RONetwork/Events/ItemEvents.swift
+++ Packages/swift-ro/Sources/RONetwork/Events/ItemEvents.swift
@@ -56,51 +56,28 @@ public enum ItemEvents {
 
 extension ItemEvents {
     public struct Thrown: Event {
-        public struct Item: Sendable {
-            public let index: Int
-            public let amount: Int
-
-            public init(index: Int, amount: Int) {
-                self.index = index
-                self.amount = amount
-            }
-        }
-
-        public let item: ItemEvents.Thrown.Item
+        public let index: Int
+        public let amount: Int
 
         init(packet: PACKET_ZC_ITEM_THROW_ACK) {
-            self.item = ItemEvents.Thrown.Item(
-                index: Int(packet.index),
-                amount: Int(packet.count)
-            )
+            self.index = Int(packet.index)
+            self.amount = Int(packet.count)
         }
     }
 }
 
 extension ItemEvents {
     public struct Used: Event {
-        public struct Item: Sendable {
-            public let index: Int
-            public let itemID: Int
-            public let amount: Int
-
-            public init(index: Int, itemID: Int, amount: Int) {
-                self.index = index
-                self.itemID = itemID
-                self.amount = amount
-            }
-        }
-
-        public let item: ItemEvents.Used.Item
+        public let index: Int
+        public let itemID: Int
+        public let amount: Int
         public let accountID: UInt32
         public let success: Bool
 
         init(packet: PACKET_ZC_USE_ITEM_ACK) {
-            self.item = ItemEvents.Used.Item(
-                index: Int(packet.index),
-                itemID: Int(packet.itemId),
-                amount: Int(packet.amount)
-            )
+            self.index = Int(packet.index)
+            self.itemID = Int(packet.itemId)
+            self.amount = Int(packet.amount)
             self.accountID = packet.AID
             self.success = (packet.result != 0)
         }
@@ -109,27 +86,15 @@ extension ItemEvents {
 
 extension ItemEvents {
     public struct Equipped: Event {
-        public struct Item: Sendable {
-            public let index: Int
-            public let location: EquipPositions
-            public let view: Int
-
-            init(index: Int, location: Int, view: Int) {
-                self.index = index
-                self.location = EquipPositions(rawValue: location)
-                self.view = view
-            }
-        }
-
-        public let item: ItemEvents.Equipped.Item
+        public let index: Int
+        public let location: EquipPositions
+        public let view: Int
         public let success: Bool
 
         init(packet: PACKET_ZC_REQ_WEAR_EQUIP_ACK) {
-            self.item = ItemEvents.Equipped.Item(
-                index: Int(packet.index),
-                location: Int(packet.wearLocation),
-                view: Int(packet.wItemSpriteNumber)
-            )
+            self.index = Int(packet.index)
+            self.location = EquipPositions(rawValue: Int(packet.wearLocation))
+            self.view = Int(packet.wItemSpriteNumber)
             self.success = (packet.result != 0)
         }
     }
@@ -137,24 +102,13 @@ extension ItemEvents {
 
 extension ItemEvents {
     public struct Unequipped: Event {
-        public struct Item: Sendable {
-            public let index: Int
-            public let location: EquipPositions
-
-            init(index: Int, location: Int) {
-                self.index = index
-                self.location = EquipPositions(rawValue: location)
-            }
-        }
-
-        public let item: ItemEvents.Unequipped.Item
+        public let index: Int
+        public let location: EquipPositions
         public let success: Bool
 
         init(packet: PACKET_ZC_REQ_TAKEOFF_EQUIP_ACK) {
-            self.item = ItemEvents.Unequipped.Item(
-                index: Int(packet.index),
-                location: Int(packet.wearLocation)
-            )
+            self.index = Int(packet.index)
+            self.location = EquipPositions(rawValue: Int(packet.wearLocation))
             self.success = (packet.flag != 0)
         }
     }
