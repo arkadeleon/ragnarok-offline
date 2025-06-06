354dc2d2dac644d2fded30e6ca2be12036356bc7
Leon Li <arkadeleon@gmail.com>
2025-06-06 11:31:44 +0800

    Updated server address regex

--- RagnarokOffline/Settings/SettingsView.swift
+++ RagnarokOffline/Settings/SettingsView.swift
@@ -74,7 +74,7 @@ struct SettingsView: View {
         }
         .onChange(of: focusedField) { oldValue, newValue in
             if oldValue == SettingsView.Field.serverAddress {
-                let regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+                let regex = /^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}|localhost)$/
                 if let _ = try? regex.wholeMatch(in: serverAddress) {
                     ClientSettings.shared.serverAddress = serverAddress
                 } else {
