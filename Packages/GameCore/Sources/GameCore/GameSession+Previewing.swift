3c632e897f01a0e31ab778255dab4fe9d46b2cce
Leon Li <arkadeleon@gmail.com>
2025-10-22 15:17:32 +0800

    Moved previewing extension to their own package

--- Packages/GameCore/Sources/GameCore/GameSession+Previewing.swift
+++ Packages/GameCore/Sources/GameCore/GameSession+Previewing.swift
@@ -1,16 +1,14 @@
 //
 //  GameSession+Previewing.swift
-//  GameView
+//  GameCore
 //
 //  Created by Leon Li on 2025/8/28.
 //
 
-import Foundation
-import GameCore
 import ResourceManagement
 
 extension GameSession {
-    static let previewing: GameSession = {
+    public static let previewing: GameSession = {
         let gameSession = GameSession(resourceManager: .previewing)
 
         let configuration = GameSession.Configuration(serverAddress: "127.0.0.1", serverPort: "6900")
@@ -18,11 +16,4 @@ extension GameSession {
 
         return gameSession
     }()
-}
-
-extension ResourceManager {
-    static let previewing = ResourceManager(
-        localURL: Bundle.main.resourceURL!,
-        remoteURL: URL(string: "http://127.0.0.1:8080/client")
-    )
 }
