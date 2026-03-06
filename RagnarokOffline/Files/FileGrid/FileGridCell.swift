8c906741f3d95a742a5a0b04b60a9f38c1f5f0f5
Leon Li <arkadeleon@gmail.com>
2026-03-06 13:46:23 +0800

    Refined byte-count formatting

--- RagnarokOffline/Files/FileGrid/FileGridCell.swift
+++ RagnarokOffline/Files/FileGrid/FileGridCell.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct FileGridCell: View {
     var file: File
 
-    @State private var subtitleKey: LocalizedStringKey?
+    @State private var subtitle: String?
 
     var body: some View {
         ImageGridCell(title: file.name) {
-            if let subtitleKey {
-                Text(subtitleKey)
+            if let subtitle {
+                Text(subtitle)
             }
         } image: {
             FileThumbnailView(file: file)
@@ -28,22 +28,10 @@ struct FileGridCell: View {
     private func loadSubtitle() async {
         if file.isDirectory {
             let fileCount = await file.fileCount()
-            subtitleKey = LocalizedStringKey("^[\(fileCount) item](inflect: true)")
+            subtitle = String(localized: LocalizedStringResource("^[\(fileCount) item](inflect: true)"))
         } else {
             let fileSize = await file.size()
-            let formatStyle = FloatingPointFormatStyle<Float>().precision(.significantDigits(2))
-            if fileSize > 1024 * 1024 * 1024 {
-                let fileSizeInGB = Float(fileSize) / 1024 / 1024 / 1024
-                subtitleKey = LocalizedStringKey("\(fileSizeInGB.formatted(formatStyle)) GB")
-            } else if fileSize > 1024 * 1024 {
-                let fileSizeInMB = Float(fileSize) / 1024 / 1024
-                subtitleKey = LocalizedStringKey("\(fileSizeInMB.formatted(formatStyle)) MB")
-            } else if fileSize > 1024 {
-                let fileSizeInKB = Float(fileSize) / 1024
-                subtitleKey = LocalizedStringKey("\(fileSizeInKB.formatted(formatStyle)) KB")
-            } else {
-                subtitleKey = LocalizedStringKey("\(fileSize.formatted()) B")
-            }
+            subtitle = fileSize.formatted(.byteCount(style: .file))
         }
     }
 }
