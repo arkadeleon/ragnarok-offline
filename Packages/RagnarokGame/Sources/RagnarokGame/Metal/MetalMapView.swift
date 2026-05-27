d98b4d8924312f9a9c339b997fb9fdd9fc98895a
Leon Li <arkadeleon@gmail.com>
2026-05-27 09:43:30 +0800

    Refactored MetalMapView to use UIViewController/NSViewController

--- Packages/RagnarokGame/Sources/RagnarokGame/Metal/MetalMapView.swift
+++ Packages/RagnarokGame/Sources/RagnarokGame/Metal/MetalMapView.swift
@@ -8,62 +8,29 @@
 import MetalKit
 import SwiftUI
 
-struct MetalMapView: View {
+#if canImport(UIKit)
+
+struct MetalMapView: UIViewControllerRepresentable {
     var scene: MapScene
 
-    var body: some View {
-        if let backend = scene.renderBackend as? MetalRenderBackend {
-            MetalMapViewContainer(scene: scene, backend: backend)
-        } else {
-            EmptyView()
+    func makeUIViewController(context: Context) -> MetalMapViewController {
+        guard let backend = scene.renderBackend as? MetalRenderBackend else {
+            preconditionFailure("scene.renderBackend must be a MetalRenderBackend.")
         }
+        return MetalMapViewController(scene: scene, backend: backend)
+    }
+
+    func updateUIViewController(_ viewController: MetalMapViewController, context: Context) {
+        viewController.update(scene: scene)
     }
 }
 
-#if canImport(UIKit)
-
-private struct MetalMapViewContainer: UIViewRepresentable {
-    var scene: MapScene
-    var backend: MetalRenderBackend
-
-    func makeUIView(context: Context) -> MapMTKHostView {
-        MapMTKHostView(scene: scene, backend: backend)
-    }
-
-    func updateUIView(_ view: MapMTKHostView, context: Context) {
-        view.update(scene: scene)
-    }
-}
-
-#elseif canImport(AppKit)
-
-private struct MetalMapViewContainer: NSViewRepresentable {
-    var scene: MapScene
-    var backend: MetalRenderBackend
-
-    func makeNSView(context: Context) -> MapMTKHostView {
-        MapMTKHostView(scene: scene, backend: backend)
-    }
-
-    func updateNSView(_ view: MapMTKHostView, context: Context) {
-        view.update(scene: scene)
-    }
-}
-
-#endif
-
-#if canImport(UIKit)
-private typealias PlatformView = UIView
-#elseif canImport(AppKit)
-private typealias PlatformView = NSView
-#endif
-
-private final class MapMTKHostView: PlatformView, MTKViewDelegate {
+final class MetalMapViewController: UIViewController, MTKViewDelegate {
     private weak var scene: MapScene?
     private let backend: MetalRenderBackend
     private let commandQueue: any MTLCommandQueue
     private let renderer: MetalMapRenderer
-    private let mtkView: MTKView
+    private var mtkView: MTKView!
 
     private var baseAzimuth: Float = 0
     private var baseElevation: Float = 0
@@ -74,33 +41,45 @@ private final class MapMTKHostView: PlatformView, MTKViewDelegate {
         self.backend = backend
         self.renderer = backend.renderer
         guard let commandQueue = renderer.device.makeCommandQueue() else {
-            fatalError("MapMTKHostView: failed to create Metal command queue")
+            fatalError("MetalMapViewController: failed to create Metal command queue")
         }
         self.commandQueue = commandQueue
-        self.mtkView = MTKView(frame: .zero, device: renderer.device)
-
-        super.init(frame: .zero)
-
-        mtkView.delegate = self
-        mtkView.colorPixelFormat = renderer.colorPixelFormat
-        mtkView.depthStencilPixelFormat = renderer.depthStencilPixelFormat
-        mtkView.clearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 0)
-
-        #if canImport(UIKit)
-        mtkView.isOpaque = false
-        mtkView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        #elseif canImport(AppKit)
-        mtkView.autoresizingMask = [.width, .height]
-        #endif
-
-        mtkView.frame = bounds
-        addSubview(mtkView)
-
-        configureGestures()
+        super.init(nibName: nil, bundle: nil)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mtkView = MTKView(frame: view.bounds, device: renderer.device)
+        mtkView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mtkView.isOpaque = false
+        mtkView.delegate = self
+        mtkView.colorPixelFormat = renderer.colorPixelFormat
+        mtkView.depthStencilPixelFormat = renderer.depthStencilPixelFormat
+        mtkView.clearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 0)
+        view.addSubview(mtkView)
+
+        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(_:)))
+        doubleTapGestureRecognizer.numberOfTapsRequired = 2
+        mtkView.addGestureRecognizer(doubleTapGestureRecognizer)
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        tapGestureRecognizer.require(toFail: doubleTapGestureRecognizer)
+        mtkView.addGestureRecognizer(tapGestureRecognizer)
+
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        mtkView.addGestureRecognizer(panGestureRecognizer)
+
+        let twoFingerPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handleTwoFingerPan(_:)))
+        twoFingerPanGestureRecognizer.minimumNumberOfTouches = 2
+        mtkView.addGestureRecognizer(twoFingerPanGestureRecognizer)
+
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
+        mtkView.addGestureRecognizer(pinchGestureRecognizer)
     }
 
     func update(scene: MapScene) {
@@ -130,37 +109,6 @@ private final class MapMTKHostView: PlatformView, MTKViewDelegate {
         commandBuffer.commit()
     }
 
-    private func configureGestures() {
-        #if canImport(UIKit)
-        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(_:)))
-        doubleTapGestureRecognizer.numberOfTapsRequired = 2
-        mtkView.addGestureRecognizer(doubleTapGestureRecognizer)
-
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        tapGestureRecognizer.require(toFail: doubleTapGestureRecognizer)
-        mtkView.addGestureRecognizer(tapGestureRecognizer)
-
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        mtkView.addGestureRecognizer(panGestureRecognizer)
-
-        let twoFingerPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handleTwoFingerPan(_:)))
-        twoFingerPanGestureRecognizer.minimumNumberOfTouches = 2
-        mtkView.addGestureRecognizer(twoFingerPanGestureRecognizer)
-
-        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
-        mtkView.addGestureRecognizer(pinchGestureRecognizer)
-        #elseif canImport(AppKit)
-        let panGestureRecognizer = NSPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        mtkView.addGestureRecognizer(panGestureRecognizer)
-
-        let magnificationGestureRecognizer = NSMagnificationGestureRecognizer(target: self, action: #selector(handleMagnification(_:)))
-        mtkView.addGestureRecognizer(magnificationGestureRecognizer)
-        #endif
-    }
-}
-
-#if canImport(UIKit)
-private extension MapMTKHostView {
     @objc func handleDoubleTap(_ gestureRecognizer: UITapGestureRecognizer) {
         guard let scene else {
             return
@@ -233,8 +181,95 @@ private extension MapMTKHostView {
         }
     }
 }
+
 #elseif canImport(AppKit)
-private extension MapMTKHostView {
+
+struct MetalMapView: NSViewControllerRepresentable {
+    var scene: MapScene
+
+    func makeNSViewController(context: Context) -> MetalMapViewController {
+        guard let backend = scene.renderBackend as? MetalRenderBackend else {
+            preconditionFailure("scene.renderBackend must be a MetalRenderBackend.")
+        }
+        return MetalMapViewController(scene: scene, backend: backend)
+    }
+
+    func updateNSViewController(_ viewController: MetalMapViewController, context: Context) {
+        viewController.update(scene: scene)
+    }
+}
+
+final class MetalMapViewController: NSViewController, MTKViewDelegate {
+    private weak var scene: MapScene?
+    private let backend: MetalRenderBackend
+    private let commandQueue: any MTLCommandQueue
+    private let renderer: MetalMapRenderer
+    private var mtkView: MTKView!
+
+    private var baseAzimuth: Float = 0
+    private var baseElevation: Float = 0
+    private var baseDistance: Float = 0
+
+    init(scene: MapScene, backend: MetalRenderBackend) {
+        self.scene = scene
+        self.backend = backend
+        self.renderer = backend.renderer
+        guard let commandQueue = renderer.device.makeCommandQueue() else {
+            fatalError("MetalMapViewController: failed to create Metal command queue")
+        }
+        self.commandQueue = commandQueue
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mtkView = MTKView(frame: view.bounds, device: renderer.device)
+        mtkView.autoresizingMask = [.width, .height]
+        mtkView.delegate = self
+        mtkView.colorPixelFormat = renderer.colorPixelFormat
+        mtkView.depthStencilPixelFormat = renderer.depthStencilPixelFormat
+        mtkView.clearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 0)
+        view.addSubview(mtkView)
+
+        let panGestureRecognizer = NSPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        mtkView.addGestureRecognizer(panGestureRecognizer)
+
+        let magnificationGestureRecognizer = NSMagnificationGestureRecognizer(target: self, action: #selector(handleMagnification(_:)))
+        mtkView.addGestureRecognizer(magnificationGestureRecognizer)
+    }
+
+    func update(scene: MapScene) {
+        self.scene = scene
+    }
+
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    }
+
+    func draw(in view: MTKView) {
+        guard let commandBuffer = commandQueue.makeCommandBuffer(),
+              let drawable = view.currentDrawable,
+              let renderPassDescriptor = view.currentRenderPassDescriptor else {
+            return
+        }
+
+        backend.prepareFrame()
+
+        renderer.render(
+            atTime: CACurrentMediaTime(),
+            viewport: view.bounds,
+            commandBuffer: commandBuffer,
+            renderPassDescriptor: renderPassDescriptor
+        )
+
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+
     override func mouseDown(with event: NSEvent) {
         var point = mtkView.convert(event.locationInWindow, from: nil)
         // NSView has bottom-left origin; flip to top-left for hit testing.
@@ -287,4 +322,5 @@ private extension MapMTKHostView {
         }
     }
 }
+
 #endif
